74babba3130f4f20e2774acf7573db6ba64c3ac3
ChenGeorgie <42752160+ChenGeorgie@users.noreply.github.com>
2021-01-14 20:42:15 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,30 +1,44 @@
-
-
-const ROW = 6;
-const COL = 6;
-const PLAYER1 = '♖';
-const PLAYER2 = '♜';
+const ROW = 8;
+const COL = 8;
+const PLAYER1 = '⛀';
+const PLAYER2 = '⛂';
+const PLAYER1DRAGROM = '⛁';
+const PLAYER2DRAGROM = '⛃';
 const OPTIONS = 'O';
 
-// document.body.appendChild(myImage);
+// get the curr pos and the next pos.
 var gFirstPos;
 var gSecondPos;
+// get the soldier string.
 var gFirstClickLogo;
+// get the empty cell the player can movment.
 var gshowTheEmptyOptionsMove;
-var gboard;
-
+// count of the soldier for each side.
+var gCountSoliderOnBoard = 0;
+var gBoard;
+// check who is turn.
 var gIsPlayer1Turn = false;
 var gIsPlayer2Turn = false;
 
+// count of players eats.
+var gPlayer1EatsCount;
+var gPlayer2EatsCount;
 
 
+// run game.
 init();
 
-
-// start run.
 function init() {
-    gboard = buildBoard();
-    renderToHtml(buildBoard());
+    // restart the players turns.
+    gIsPlayer1Turn = false;
+    gIsPlayer2Turn = false;
+    // restart the players eat another.
+    gPlayer1EatsCount = 0;
+    gPlayer2EatsCount = 0;
+    //get the board.
+    gBoard = buildBoard();
+    // show the board.
+    renderToHtml(gBoard);
 }
 
 function renderToHtml(board) {
@@ -50,6 +64,16 @@ function renderToHtml(board) {
     strHtml += `</tr>`;
     // close table.
     strHtml += `</table>`;
+    var player1Eats = `player1 eats: <span class="player-eats-count">${gPlayer1EatsCount}</span>`;
+    var player2Eats = `player2 eats: <span class="player-eats-count">${gPlayer2EatsCount}</span>`;
+    strHtml += `<div class="player-eats">${player1Eats} ${player2Eats}</div>`
+        // show the game over modal who win.
+    strHtml += `<div class="game-over">Game Over
+     <span class="who-win">${((gPlayer2EatsCount === gCountSoliderOnBoard) ? 'Player2 Win' : '')}</span>   
+     <span class="who win">${((gPlayer1EatsCount === gCountSoliderOnBoard) ? 'Player1 Win' : '')}</span>
+     <button onClick="isClickOnRestartGame()" class="restart-game">RESTART GAME</<button>
+    </div>`
+
 
     var eLcontainer = document.querySelector('.container');
     eLcontainer.innerHTML = strHtml;
@@ -71,90 +95,93 @@ function buildBoard() {
 
 }
 
+
+
 function cell(elCell, i, j) {
-    // console.log('first player click:', gIsPlayer1Turn);
+
+    // console.log(elCell)
+
+
+    gSecondPos = [];
     // main dom of all the movement.
-    var currCell = gboard[i][j];
-    
-    // check if the curr clicked 
+    var currCell = gBoard[i][j];
+
+    // check if the correct for player1 clicked (not empty cell).
     if (currCell === PLAYER1 && !gIsPlayer2Turn) {
+
         gIsPlayer1Turn = true;
         // get options for player to move.
         getCurrOptionsNeighbors(currCell, i, j, false, true)
     } else {
+        // check if the correct for player2 clicked (not empty cell).
         if (currCell === PLAYER2 && gIsPlayer2Turn) {
+            // console.log('Player2');
             gIsPlayer1Turn = false;
             // get options for player to move.
             getCurrOptionsNeighbors(currCell, i, j, true, false);
         }
     }
 
-
-    // update modal.
     // when player click on one of the options to move.
-    moveTheSoliderToCurrOption(currCell, i, j);
+    getNextPosOptions(currCell, i, j);
 
     // update dom when player EAT.
     // update dom.
-    updateDomWhenPlayerEat(i, j);
+    updateDomWhenPlayer1Eat(i, j);
 
-    // console.log(gboard);
-    gSecondPos = [];
-    renderToHtml(gboard);
+
+    // // console.log(gBoard);
+    renderToHtml(gBoard);
+
+    checkWin();
+
+    // elCell.style.backgroundColor = "red";
 }
 
-function updateDomWhenPlayerEat() {
+
+function updateDomWhenPlayer1Eat() {
 
     if (gIsPlayer1Turn) {
 
-
-        // console.warn('check!');
         // only if the player start to move.
         if (gSecondPos) {
-            // when player1 click to eat to left side.
+            // when player1 click to eat to LEFT side.
             player1Eat(2, 2, 1, 1);
-            // // when player1 click to eat to right side.
-            player1Eat(2, 2, 1, 1)
-            // if (gFirstPos.i + 2 === gSecondPos.i && gFirstPos.j - 2 === gSecondPos.j) {
-            //     gboard[gFirstPos.i+1][gFirstPos.j-1] = '';
-            //     console.log('eat!!!')
-            // } 
-
-            // // when player1 eat to right side.
-            // if (gFirstPos.i + 2 === gSecondPos.i && gFirstPos.j + 2 === gSecondPos.j) {
-            //     // clear the soldier the player eat.
-            //     gboard[gFirstPos.i+1][gFirstPos.j+1] = '';
-            //     console.log('eat!!!')
-            // }
-        }
-        // TODO: when player2 eat the player1, this player1 gone from board.
-
-    } else {
-        if (gIsPlayer2Turn) {
-            if (gSecondPos) {
-                console.log('last pos: ', gSecondPos);
-                console.log('curr pos: ', gFirstPos);
-            }
-            console.log('----------')
+            // // when player1 click to eat to RIGHT side.
+            player1Eat(2, -2, 1, -1)
         }
     }
-    // console.log('last pos: ', gSecondPos);
-    // console.log('curr pos: ', gFirstPos);
 }
-
+// move to the correct eat and reomove that player1 eat.
 function player1Eat(iGoTo, jGoTo, iDelete, jDelete) {
-    // check if has a options to eat.
+    // check if has a options to eat. this player is eat?
     var checkForI = (gFirstPos.i + iGoTo === gSecondPos.i);
     var checkForJ = (gFirstPos.j - jGoTo === gSecondPos.j);
 
     if (checkForI && checkForJ) {
-        gboard[gFirstPos.i + iDelete][gFirstPos.j - jDelete] = '';
+        // count the player1 eats player2.
+        gPlayer1EatsCount++;
+        gBoard[gFirstPos.i + iDelete][gFirstPos.j - jDelete] = '';
+    }
+}
+// move to the correct eat and reomove that player2
+
+function player2Eat(iGoTo, jGoTo, iDelete, jDelete) {
+    // only if the player eat(jump over the player).
+    var checkForI = (gFirstPos.i - iGoTo === gSecondPos.i);
+    var checkForJ = (gFirstPos.j + jGoTo === gSecondPos.j);
+
+    // player eat.
+    if (checkForI && checkForJ) {
+        // count the player2 eats player1.
+        gPlayer2EatsCount++;
+        // clear from board the soldier he is eat.
+        gBoard[gFirstPos.i - iDelete][gFirstPos.j + jDelete] = '';
     }
 }
 
-
-function moveTheSoliderToCurrOption(currCell, i, j) {
-
+function getNextPosOptions(currCell, i, j) {
+    // console.log('last pos: ', gFirstPos);
     // this is  a next click after click the on the curr soldier player.
     if (currCell === OPTIONS) {
 
@@ -162,31 +189,68 @@ function moveTheSoliderToCurrOption(currCell, i, j) {
         if (gIsPlayer1Turn === true) gIsPlayer2Turn = true; // player1
         if (gIsPlayer1Turn === false) gIsPlayer2Turn = false; // player2
 
-        // get the pos that player click. 
+        // get the pos that player click to move. 
         gSecondPos = { i: i, j: j };
 
+        // console.log('curr pos: ', gSecondPos);
+
+        // when player2 click to eat to RIGHT side.
+        player2Eat(2, 2, 1, 1);
+        // when player2 click to eat to LEFT side.
+        player2Eat(2, -2, 1, -1);
+
         // remove the first player pos from dom.
-        gboard[gFirstPos.i][gFirstPos.j] = '';
-        // gboard[gSecondPos.i+2][gSecondPos.j-2] = 'j';
-        gboard[gSecondPos.i][gSecondPos.j] = gFirstClickLogo;
+        gBoard[gFirstPos.i][gFirstPos.j] = '';
+        gBoard[gSecondPos.i][gSecondPos.j] = gFirstClickLogo;
 
         // when we move to next location we clear the all rest options.
         clearOptionsAfterMove();
     }
 }
 
+function checkWin() {
+    // console.log('maximum soldier:', gCountSoliderOnBoard);
+    // console.log('player1 eats soldier:', gPlayer1EatsCount);
+    // console.log('player1 eats soldier:', gPlayer2EatsCount);
+
+    // check if player1 win.
+    if (gPlayer1EatsCount === gCountSoliderOnBoard) {
+        var gameOverModal = document.querySelector('.game-over');
+        console.warn('player1 win')
+
+        // show the game over modal.
+        gameOverModal.style.display = 'block';
+
+    } else {
+        // check if player2 win.
+        if (gPlayer2EatsCount === gCountSoliderOnBoard) {
+            var gameOverModal = document.querySelector('.game-over');
+            console.warn('player2 win')
+
+            // show the game over modal.
+            gameOverModal.style.display = 'block';
+
+        }
+    }
+
+}
+
+// when game is over and the user click on restart game. 
+function isClickOnRestartGame() {
+    init();
+}
+
+
 //  when user click on player we check for the neighbors positions.
 function getCurrOptionsNeighbors(currCell, i, j, isBottomTurn, isTopTurn) {
+    // clear the option (if have rest).
     clearOptionsAfterMove();
+    // get the next location.
     gFirstPos = { i: i, j: j };
-    gshowTheEmptyOptionsMove = neighbors(gboard, gFirstPos, isBottomTurn, isTopTurn);
-    // // only when player1 play.
-    // if (gIsPlayer1Turn) {
-    //     console.log('curr pos:', gFirstPos)
-    //     console.table(gshowTheEmptyOptionsMove)
-    //     console.log('-------')
+    // get the empty cell location.
+    gshowTheEmptyOptionsMove = neighbors(gBoard, gFirstPos, isBottomTurn, isTopTurn);
 
-    // }
+    // save the first logo of cell.
     gFirstClickLogo = currCell;
 
     // update dom.
@@ -206,9 +270,15 @@ function placeThePlayerOnBoard(board) {
             board[board.length - 1][j] = PLAYER2;
             board[board.length - 2][j] = PLAYER2;
 
+            // count how many soldier have each player.
+            if (board[i][j] === PLAYER1) {
+                gCountSoliderOnBoard++
+            }
+
         }
     }
 }
+
 function showTheEmptyOptions(gshowTheEmptyOptionsMove) {
     // show -  only if a empty cell neighbors. 
     if (gshowTheEmptyOptionsMove) {
@@ -217,7 +287,7 @@ function showTheEmptyOptions(gshowTheEmptyOptionsMove) {
 
             // change the cell to the options.
             // update modal and dom.
-            gboard[currEmptyOption.i][currEmptyOption.j] = OPTIONS;
+            gBoard[currEmptyOption.i][currEmptyOption.j] = OPTIONS;
 
             // get the elements- of the options(this append when we click on soldier).
             var currTd = document.querySelector(`.col-${currEmptyOption.i}-${currEmptyOption.j}`);
@@ -230,11 +300,11 @@ function showTheEmptyOptions(gshowTheEmptyOptionsMove) {
 
 // check if have a options to move on the board and clean it..
 function clearOptionsAfterMove() {
-    for (var i = 0; i < gboard.length; i++) {
-        for (var j = 0; j < gboard[0].length; j++) {
-            var currCell = gboard[i][j];
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[0].length; j++) {
+            var currCell = gBoard[i][j];
             if (currCell === OPTIONS) {
-                gboard[i][j] = '';
+                gBoard[i][j] = '';
             }
         }
     }
@@ -313,5 +383,3 @@ function neighbors(mat, pos, isBottomTurn, isTopTurn) {
     return emptyOptions;
 
 }
-
-
